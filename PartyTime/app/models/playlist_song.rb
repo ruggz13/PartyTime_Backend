00066a83a80841654c5296757a91d8b0dc428932
ruggz13 <nicolasruggieri7@gmail.com>
2019-09-17 15:08:10 -0400

    added relationships

--- PartyTime/app/models/playlist_song.rb
+++ PartyTime/app/models/playlist_song.rb
@@ -1,2 +1,4 @@
 class PlaylistSong < ApplicationRecord
+    belongs_to :playlist_song
+    belongs_to :song
 end
--- PartyTime/config/routes.rb
+++ PartyTime/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :create, :update, :destroy]
+  resources :songs, only: [:index, :show]
+  resources :playlist, only: [:index, :show, :create, :update, :destroy]
+  resources :playlist_songs, only: [:index, :show, :update, :destroy]
 end
